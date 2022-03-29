dd68836b19a37afe2c9295a7a1bdec8696fbe7f9
dbBurzum <khalidsr@code.edu.az>
2022-03-30 03:08:17 +0400

    Add project files.

---
+++ UserAccount/Models/User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAccount.Models
+{
+    internal class User:IAccount
+    {
+        private static int _id=1;
+        public int Id { get; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        private string _password;
+        
+        public string Password {
+            get 
+            {
+             return _password;
+            } 
+            set
+            {
+                if (PasswordChecker(value)==true)
+                {
+                    _password = value;
+                    _id++;
+                    ShowInfo();
+
+                    return;
+
+                }
+                Console.WriteLine("=================================================================================");
+                Console.WriteLine("Password is wrong. Correct form: More than 8, 1 uppercase, 1 lowercase, 1 number");
+                return;
+
+            } }
+
+        /*public readonly int id=0;*/
+
+        public User()
+        {
+            
+            Id= _id;
+        }
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+        public bool PasswordChecker(string password)
+        {
+            char[] pas = password.ToCharArray();
+            bool pascheck = false;
+
+            if (pas.Length >= 8)
+            {
+                for (int i = 0; i < pas.Length; i++)
+                {
+                    if (char.IsLower(pas[i]) == true)
+                    {
+                        for (int j = 0; j < pas.Length; j++)
+                        {
+                            if (char.IsUpper(pas[j]) == true)
+                            {
+                                for (int k = 0; k < pas.Length; k++)
+                                {
+                                    if (char.IsNumber(pas[k]) == true)
+                                    {
+                                        pascheck = true;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return pascheck;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Your id is {Id} \n" +
+                $"Your full name: {FullName} \n" +
+                $"Your email: {Email}");
+        }
+    }
+
+    interface IAccount
+    {
+        bool PasswordChecker(string password);
+        void ShowInfo();
+    }
+}
---
+++ UserAccount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UserAccount.Models;
+namespace UserAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            do
+            {
+                User user = new User();
+                Console.Write("Enter your full name: ");
+                user.FullName = Console.ReadLine();
+
+                Console.Write("Enter your email: ");
+                user.Email = Console.ReadLine();
+
+                Console.Write("Enter your password: ");
+                user.Password = Console.ReadLine();
+
+                Console.WriteLine("Press any key to continue");
+                Console.WriteLine("Press ESC to quit the app");
+            } while (Console.ReadKey().Key !=ConsoleKey.Escape);
+        }
+    }
+}
