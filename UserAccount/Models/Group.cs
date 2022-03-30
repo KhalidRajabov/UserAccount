11e2b61b6896add96b5d0e0e1946f8f80f64f1b9
dbBurzum <khalidsr@code.edu.az>
2022-03-31 02:12:05 +0400

    Updated: Group class

--- UserAccount/Models/Group.cs
+++ UserAccount/Models/Group.cs
@@ -8,6 +8,61 @@ namespace UserAccount.Models
 {
     internal class Group
     {
-        public int student;
+        public string GroupNo;
+        public int StdLimit;
+        public Group(string groupno,int stdlimit)
+        {
+            GroupNo=groupno;
+            StdLimit=stdlimit;
+        }
+
+        Student [] Students = new Student[0];
+
+        public bool CheckGroupNo(string groupNo)
+        {
+            bool final=false;
+            bool let;
+            bool num;
+            if (groupNo.Length==5)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    let = false;
+                    if (char.IsUpper(groupNo[i])==true)
+                    {
+                        let = true;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("First 2 letters of GroupNo must be in uppercase.");
+                        break;
+                    }
+                }
+                for (int i = 2; i < groupNo.Length; i++)
+                {
+                    num = false;
+                    if (char.IsNumber(groupNo[i])==true)
+                    {
+                        num = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("GroupNo must have numbers in it.");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("GroupNo must have 5 characters: 2 uppercase and 3 numbers");
+            }
+           
+            final=true;
+            return final;
+
+        }
     }
+
+
 }
